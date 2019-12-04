847d3b39931eff57d7535f51d02996fee70d4430
Syeda <mnzl258@gmail.com>
2019-12-04 13:09:42 -0500

    modfied Typeahead

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import 'react-bootstrap-typeahead/css/Typeahead-bs4.css';
-import {MenuItem,menuItemContainer, Menu, Typeahead, Highlighter} from 'react-bootstrap-typeahead';
+import { MenuItem, menuItemContainer, Menu, Typeahead, Highlighter } from 'react-bootstrap-typeahead';
 import Header from './components/Header';
 import CityList from './components/CityList';
-import {cities} from './db.js';
+import { cities } from './db.js';
 import './styles/SearchBar.css';
 
 const TypeaheadMenuItem = menuItemContainer(MenuItem);
 
-function App() {
-  let state = {
-    selected: []
-  }
-  return (
 
-    <div>
-      <Header title="Weather Monster"/>
-       <div className='search-bar'>
+
+class App extends Component {
+  state = {
+    selected: [],
+    history: []
+  }
+  render() {
+    return (
+      <div>
+        <Header title="Weather Monster" />
+        <div className='search-bar'>
           <Typeahead id='sample'
-          multiple 
-          options= {cities.map((city)=> (city.name))} placeholder="Enter a city..."
-          onChange={(selected) => {
-          console.log('hi')
-          }}
-          selected={state.selected}
-          
+            multiple
+            options={cities.map((city) => (city.name))} placeholder="Enter a city..."
+            onChange={(selected) => {
+              if (selected.length >= 1) {
+                console.log(selected)
+                this.setState({...this.state, history: this.state.history.concat(selected)})
+              }
+            }}
+            selected={this.state.selected}
+            renderMenuItemChildren={(option, props, index) => {
+            return (<span className='ta-item'  onClick={() => console.log("hi")}>{option}</span>)
+            }}
           />
-      </div> 
-    </div>
-  );
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
-
-
 
 
 
